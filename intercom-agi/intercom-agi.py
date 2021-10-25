fa1e53057daadc1b575991f03f4e827a8db74ef8
Itay S <5647956+itay-sho@users.noreply.github.com>
2021-10-25 21:41:21 +0300

    lots of progress

--- intercom-agi/intercom-agi.py
+++ intercom-agi/intercom-agi.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
-import typing
+#!/usr/local/bin/python3.10
 
 from asterisk.agi import *
+from telebot import types, AdvancedCustomFilter
+
 import telebot
 import ffmpeg
 import sys
@@ -9,10 +10,24 @@ import enum
 import pathlib
 import tempfile
 import json
+import typing
 
 
 class UnknownExtensionError(Exception):
     pass
+
+
+class ChatFilter(AdvancedCustomFilter):
+    """
+    Check whether chat_id corresponds to given chat_id.
+    Example:
+    @bot.message_handler(chat_id=[99999])
+    """
+
+    key = 'chat_ids'
+
+    def check(self, message, allowed_chats):
+        return message.chat.id in allowed_chats
 
 
 @enum.unique
@@ -30,7 +45,7 @@ class ReturnValues(enum.Enum):
 
 
 def load_secrets() -> dict:
-    with open('../intercom-agi/secrets.json') as f:
+    with open('secrets.json') as f:
         return json.loads(f.read())
 
 
@@ -41,13 +56,31 @@ def init_agi() -> None:
 
 def init_telegram_bot(secrets_dict: dict) -> telebot.TeleBot:
     tb = telebot.TeleBot(secrets_dict['token'])
-    tb.send_message(secrets_dict['chat_id'], 'Bot is up')
+    tb.threaded = False
+
+    @tb.message_handler(chat_ids=[secrets_dict['chat_id']], commands=['open'])
+    def open_door(message):
+        tb.reply_to(message, '5')
+
+    @tb.message_handler(chat_ids=[secrets_dict['chat_id']], commands=['reject'])
+    def reject(message):
+        tb.reply_to(message, 'rejected')
+
+    tb.add_custom_filter(ChatFilter())
 
     return tb
 
 
 def send_telegram_recording(tb: telebot.TeleBot, chat_id: int, recording: typing.BinaryIO) -> None:
+    # TODO: maybe remove this function?
     tb.send_voice(chat_id, voice=recording)
+
+
+def _get_response_keyboard() -> types.ReplyKeyboardMarkup:
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.row(types.KeyboardButton('/open'), types.KeyboardButton('/reject'))
+
+    return markup
 
 
 def get_record_filename_as_m4a() -> str:
@@ -62,7 +95,7 @@ def get_record_filename_as_m4a() -> str:
                 .input(original_filename)
                 .output(output_mp4_filename)
                 .overwrite_output()
-                .run(capture_stdout=True)
+                .run(capture_stdout=True, capture_stderr=True)
             )
 
             return output_mp4_filename
@@ -81,8 +114,18 @@ def main() -> int:
     try:
         recording_filename = get_record_filename_as_m4a()
 
+        # send the new recording to the chat
         with open(recording_filename, 'rb') as f:
             send_telegram_recording(tb, secrets_dict['chat_id'], f)
+
+        # change output keyboard
+        tb.send_message(
+            secrets_dict['chat_id'],
+            'Would you like to open? (20 seconds to answer):',
+            reply_markup=_get_response_keyboard()
+        )
+
+        tb.infinity_polling(skip_pending=True)
 
     except FileNotFoundError:
         return ReturnValues.RECORD_FILENAME_NOT_FOUND.value
